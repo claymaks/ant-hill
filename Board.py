06228da98968c9e1f33772ef4500e0d949309ceb
cmaks <cmaks@umich.edu>
2019-07-09 15:45:10 -0400

    RME Layout

---
+++ Board.py
@@ -0,0 +1,14 @@
+
+
+class Board(object):
+
+    def __init__(self):
+        self.array = []
+        pass
+
+
+    #REQUIRES: Pixel object
+    #MODIFIES: self.array
+    #EFFECTS : Inserts Pixel object at x,y coord given by pixel
+    def insert_pixel(Pixel):
+        pass
